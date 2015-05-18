827eee40fd9f9a9488f60e3b86afc596ebed648c
joanna-bona <joanna.bonaa@gmail.com>
2015-05-18 08:09:11 +0200

    czesciowe spiecie modelu gui z modelem macierzowy + zaproponowany nowy dodatkowy model reprezentacji sieci

---
+++ src/main/java/org/petri/nets/model/Transition.java
@@ -0,0 +1,7 @@
+package org.petri.nets.model;
+
+/**
+ * Created by Asia on 2015-05-17.
+ */
+public class Transition {
+}
---
+++ src/main/java/org/petri/nets/service/SynchronizeService.java
@@ -0,0 +1,7 @@
+package org.petri.nets.service;
+
+/**
+ * Created by Asia on 2015-05-17.
+ */
+public interface SynchronizeService {
+}
