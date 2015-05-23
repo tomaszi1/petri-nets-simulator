d84fe0c109484748968ec47b766be21332b03b1e
joanna-bona <joanna.bonaa@gmail.com>
2015-05-23 17:50:23 +0200

    dodawanie lukow

--- src/main/java/org/petri/nets/service/SynchronizeServiceImpl.java
+++ src/main/java/org/petri/nets/service/SynchronizeServiceImpl.java
@@ -1,5 +1,6 @@
 package org.petri.nets.service;
 
+import org.petri.nets.model.Arc;
 import org.petri.nets.model.DomainModel;
 import org.petri.nets.model.Place;
 import org.petri.nets.model.Transition;
@@ -30,10 +31,46 @@ public class SynchronizeServiceImpl implements SynchronizeService{
 
     @Override
     public void addArc(Place place, Transition transition, boolean isPlaceStart) {
-
+        boolean transitFromPlace =  true;
+        boolean placeFromTransition = false;
+        if(isPlaceStart){
+            transitFromPlace = false;
+            placeFromTransition = true;
+        }
+        addTransitToPlace(place,transition,transitFromPlace);
+        addPlaceToTransit(place,transition, placeFromTransition);
 
     }
+    /**
+     * ostatni paramter pyta czy jakie przejscie dodajemy. Czy można z przejscia dojsc do miejsca
+     * jesli true to dodajemy do transitionFrom;
+     * false -> transitionTo;
+     **/
+    private void addTransitToPlace(Place place, Transition transition, boolean transitFromPlace){
 
+        Arc arc = new Arc(0,0);
+
+        if(transitFromPlace){
+            model.getPetriNet().getPlaceMap().get(place.getIdPlace()).getTransitionFrom().put(transition,arc);
+        }else{
+            model.getPetriNet().getPlaceMap().get(place.getIdPlace()).getTransitionTo().put(transition,arc);
+        }
+    }
+    /**
+     * ostatni paramter pyta czy jakie miejsce dodajemy. Czy można z miejsca dojsc do przejscia
+     * jesli true to dodajemy do placeFrom;
+     * false -> placeTo;
+     **/
+    private void addPlaceToTransit(Place place, Transition transition, boolean placeFromTransition){
+
+        Arc arc = new Arc(0,0);
+
+        if(placeFromTransition){
+            model.getPetriNet().getTransitionMap().get(transition.getIdTransition()).getPlaceFrom().put(place,arc);
+        }else{
+            model.getPetriNet().getTransitionMap().get(transition.getIdTransition()).getPlaceTo().put(place,arc);
+        }
+    }
 
     @Override
     public void removePlace(Place place) {
