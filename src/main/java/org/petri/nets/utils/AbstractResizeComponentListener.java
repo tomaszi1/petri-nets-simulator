7ee4f3774757945d6619e10354ff607cbbba95fd
Tomasz Idziak <tomasz.idziak@gmail.com>
2015-05-30 16:26:51 +0200

    AbstractResizeComponentListener.java

---
+++ src/main/java/org/petri/nets/utils/AbstractResizeComponentListener.java
@@ -0,0 +1,25 @@
+package org.petri.nets.utils;
+
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+
+/**
+ * Created by Tomasz on 2015-05-30.
+ */
+public abstract class AbstractResizeComponentListener implements ComponentListener {
+
+    @Override
+    public void componentMoved(ComponentEvent e) {
+
+    }
+
+    @Override
+    public void componentShown(ComponentEvent e) {
+
+    }
+
+    @Override
+    public void componentHidden(ComponentEvent e) {
+
+    }
+}
