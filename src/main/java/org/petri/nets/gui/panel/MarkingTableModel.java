9d1b42fcea07b91711c1c360076307ec39c89052
joanna-bona <joanna.bonaa@gmail.com>
2015-05-31 13:15:02 +0200

    polaczenie gui i wskaznikow poczatkowych

--- src/main/java/org/petri/nets/gui/panel/MarkingTableModel.java
+++ src/main/java/org/petri/nets/gui/panel/MarkingTableModel.java
@@ -5,7 +5,7 @@ import org.petri.nets.model.PetriNet;
 import org.petri.nets.model.Place;
 
 import javax.swing.table.AbstractTableModel;
-import java.util.HashMap;
+import java.util.ArrayList;
 
 
 /**
@@ -13,9 +13,11 @@ import java.util.HashMap;
  */
 public class MarkingTableModel extends AbstractTableModel {
     private DomainModel domainModel ;
+    private ArrayList<Integer[]> initialMarkingListGUI = new ArrayList<Integer[]>();
     public MarkingTableModel (DomainModel domainModel){
         this.domainModel = domainModel;
     }
+
     @Override
     public int getRowCount() {
         return 1;
@@ -28,24 +30,19 @@ public class MarkingTableModel extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int row, int col) {
-        PetriNet petriNet = getDomainModel().getPetriNet();
+        //PetriNet petriNet = getDomainModel().getPetriNet();
 
-        Place place = getPlaceByColumId(getColumnName(col),petriNet);
-        return getDomainModel().getPetriNet().getInitialMarking().get(place);
+        //Place place = getPlaceByColumId(getColumnName(col),petriNet);
+        int marking = initialMarkingListGUI.get(col)[1];
+        return marking;
     }
 
     @Override
     public String getColumnName(int col) {
-        HashMap<Integer, Place> placeMap = getDomainModel().getPetriNet().getPlaceMap();
-        String id = "P";
-        do{
-            if(placeMap.get(col) != null){
-                id += placeMap.get(col).getIdPlace() + 1;
-            }
-            col++;
-        }while(placeMap.get(col-1) == null && col-1<placeMap.size());
-
-        return id ;
+        //HashMap<Integer, Place> placeMap = getDomainModel().getPetriNet().getPlaceMap();
+        String sufix = "P";
+        int idPlace = initialMarkingListGUI.get(col)[0] + 1;
+        return sufix + idPlace ;
     }
 
     @Override
@@ -63,10 +60,10 @@ public class MarkingTableModel extends AbstractTableModel {
         try {
             PetriNet petriNet = getDomainModel().getPetriNet();
             //Place place = getPlaceByColumId(getColumnName(col), petriNet);
-            Integer placeId = getPlaceId(getColumnName(col),petriNet);
+            Integer placeId = initialMarkingListGUI.get(col)[0];
             if (value instanceof String) {
-
                 petriNet.setInitialMarking(placeId, Integer.parseInt((String) value));
+                initialMarkingListGUI.get(col)[1]= Integer.parseInt((String) value);
             } else if (!(value instanceof Integer))
                 return;
             else {
@@ -74,6 +71,7 @@ public class MarkingTableModel extends AbstractTableModel {
                 if (intval < 0)
                     return;
                 petriNet.setInitialMarking(placeId, (Integer) value);
+                initialMarkingListGUI.get(col)[1]= (Integer) value;
             }
             fireTableCellUpdated(row, col);
 
@@ -84,10 +82,14 @@ public class MarkingTableModel extends AbstractTableModel {
 
     public void addNewMarking(Place place, int marking){
         domainModel.getPetriNet().getInitialMarking().put(place.getIdPlace(),marking);
+        Integer[] markingList = {place.getIdPlace(),marking};
+        initialMarkingListGUI.add(markingList);
+        //idPlace.add(place.getIdPlace());
         fireTableStructureChanged();
     }
     public void removeMarking(Place place){
         domainModel.getPetriNet().getInitialMarking().remove(place.getIdPlace());
+        removeFromlistGUI(place.getIdPlace());
         fireTableStructureChanged();
     }
     public DomainModel getDomainModel() {
@@ -95,6 +97,7 @@ public class MarkingTableModel extends AbstractTableModel {
     }
 
     public void setDomainModel(DomainModel domainModel) {
+
         this.domainModel = domainModel;
     }
     private Integer getPlaceId(String name, PetriNet petriNet){
@@ -105,5 +108,21 @@ public class MarkingTableModel extends AbstractTableModel {
         Integer id = new Integer(name.substring(1));
         Place place = petriNet.getPlaceMap().get(id-1);
         return place;
+    }
+
+    public ArrayList<Integer[]> getInitialMarkingListGUI() {
+        return initialMarkingListGUI;
+    }
+
+    public void setInitialMarkingListGUI(ArrayList<Integer[]> initialMarkingListGUI) {
+        this.initialMarkingListGUI = initialMarkingListGUI;
+    }
+    private void removeFromlistGUI(int idPlace){
+        for(int i= 0; i< initialMarkingListGUI.size(); i++){
+            if(initialMarkingListGUI.get(i)[0]==idPlace){
+                initialMarkingListGUI.remove(i);
+                return;
+            }
+        }
     }
 }
